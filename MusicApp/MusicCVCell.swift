6aa7393d388c06149774a9689142b9dfab62c85a
Felipe Augusto Silva <felipeas@ciant.com>
2022-02-09 00:48:54 -0300

    feature: Setting up collection View

---
+++ MusicApp/MusicCVCell.swift
@@ -0,0 +1,25 @@
+//
+//  MusicCVCell.swift
+//  MusicApp
+//
+//  Created by FELIPE AUGUSTO SILVA on 08/02/22.
+//
+
+import UIKit
+
+class MusicCVCell: UICollectionViewCell {
+
+//    @IBOutlet weak var MusicTitle: UILabel!
+//    @IBOutlet weak var bandTitle: UILabel!
+//    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var bandTitle: UILabel!
+    @IBOutlet weak var MusicTitle: UILabel!
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        
+    }
+
+}
--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MusicCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        MusicCollectionView.register(UINib(nibName: "MusicCVCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+ 
     }
-
-
 }
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = MusicCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MusicCVCell
+
+        return cell
+    }
+}
+
 
